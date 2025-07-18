10d5c4ba73f2108f1c49648b364af21b15253b38
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-07-18 17:32:42 -0400

    fin

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { createJSONStorage, devtools, persist } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import type { DraftUser, User } from '../types'
 import { v4 as uuidv4 } from 'uuid'
 
